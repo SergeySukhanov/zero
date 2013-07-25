ead097192899205338f1537d74abfe8245358176
SergeySukhanov <sergey.n.sukhanov@gmail.com>
2013-07-25 18:19:51 +0400

    1222

--- js/vader/vader-home-dashboardjsgraph/vader-home-dashboardjsgraph.js
+++ js/vader/vader-home-dashboardjsgraph/vader-home-dashboardjsgraph.js
@@ -26,15 +26,15 @@ YUI.add('vader-home-dashboardjsgraph', function (Y) {
 
             var container = Y.one('#body');
             container.setHTML(
-                '<div id="placeholder" class="demo-placeholder" style="float:left; width:100%; height:425px;"></div>'+
-                '<div class="smallGraph"><h1>Sleep Efficiency 33%</h1><p>Last night slept for 3hr18m</p>'+
-                   '<div id="placeholderSleep" class="demo-placeholder" style="float:left; width:32%; height:200px;"></div>'+
+                '<div id="placeholder" class="demo-placeholder" style="float:left; width:100%; height:410px;"></div>'+
+                '<div class="smallGraph first"><h1>Sleep Efficiency 33%</h1><p>Last night slept for 3hr18m</p>'+
+                   '<div id="placeholderSleep" class="demo-placeholder" style="float:left; width:100%; height:140px;"></div>'+
                 '</div>'+
                 '<div class="smallGraph"><h1>Eating Habits</h1><p>Higt Carb diet</p>'+
-                   '<div id="placeholderEat" class="demo-placeholder" style="float:left; width:32%; height:200px;"></div>'+
+                   '<div id="placeholderEat" class="demo-placeholder" style="float:left; width:100%; height:140px;"></div>'+
                 '</div>'+
                 '<div class="smallGraph"><h1>Stress vs Wellness</h1><p>Recommend the members to take a vacation</p>'+
-                   '<div id="placeholderStress" class="demo-placeholder" style="float:left; width:32%; height:200px;"></div>'+
+                   '<div id="placeholderStress" class="demo-placeholder" style="float:left; width:100%; height:140px;"></div>'+
                 '</div>'
 //                    +
 //                '<div class="clearfix"></div>' +
